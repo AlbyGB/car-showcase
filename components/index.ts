f5e78ecef7d65d47a4a679d2642432a1c24d79ab
albyg <alby0705@gmail.com>
2023-09-01 18:47:06 +0200

    first commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from './Hero';
+import CustomButton from "@/components/CustomButton";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export {
+    Hero,
+    CustomButton,
+    Footer,
+    Navbar
+}
